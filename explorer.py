4257a659061055e4d27f31973bbd29b349ed0b85
Hector Zambrano <zambranohector2002@gmail.com>
2023-07-30 17:31:25 -0500

    <feat>(<>): <description>
    add the archive main
    
    <body>
    
    <footer>
    
    future
    configure the gui
    export exe for w10
    
    By HectorZL

--- explorer.py
+++ explorer.py
@@ -1,32 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
-url = "http://lamansion-crg.net/forum/index.php"  # URL de la página
-comment_selector = ".postcolor .post-entry a"  # Selector CSS para los enlaces del primer comentario
+# Function to get href values
+def get_href_values(url):
+    # Send a GET request to the URL
+    response = requests.get(url)
+    
+    # Parse the HTML content of the page with BeautifulSoup
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # Find all 'div' tags with align="center" in the document
+    div_tags = soup.find_all('div', {'align': 'center'})
+    
+    # Create a set to store unique href values
+    href_set = set()
+    
+    # For each 'div' tag found, find 'a' tags within and add the 'href' attribute to the set
+    for tag in div_tags:
+        a_tags = tag.find_all('a')
+        for a_tag in a_tags:
+            href_value = a_tag.get('href')
+            # Only add href values that start with "ed2k"
+            if href_value.startswith("ed2k"):
+                href_set.add(href_value)
+    
+    # Order the href values
+    href_list = sorted(list(href_set))
+    
+    # Export the href values to a text file
+    with open('href_values.txt', 'w') as file:
+        for href_value in href_list:
+            file.write(href_value + '\n')
+    
+    # Print the unique href values
+    for href_value in href_list:
+        print(href_value)
 
-# Obtener el contenido HTML de la página
-response = requests.get(url)
-html = response.text
-
-# Analizar el HTML con BeautifulSoup
-soup = BeautifulSoup(html, "html.parser")
-
-# Encontrar el primer comentario y obtener todos los enlaces dentro de él
-comment = soup.select(comment_selector)
-links = [a["href"] for a in comment]
-
-# Configurar Selenium para automatizar el navegador Brave
-options = Options()
-options.binary_location = '/bin/brave-browser'  
-driver_path = '/path/to/chromedriver'  # Reemplaza esto con la ubicación real del archivo ejecutable del controlador de Chrome
-driver = webdriver.Chrome(executable_path=driver_path, options=options)
-
-# Abrir cada enlace en una ventana del navegador
-for link in links:
-    driver.get(link)
-
-# Cerrar el navegador
-driver.quit()
-
+# Call the function with your URL
+get_href_values('http://lamansion-crg.net/forum/index.php?showtopic=96333')
